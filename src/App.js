bf0b99fd4de90d845b2e2201ba3db0f1f5b8527e
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-15 23:08:12 -0500

    add googlefonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+
+h1,h2,h3 {
+  font-family: 'Righteous', cursive;
+}
+`;
 
 function App() {
-  return <div>Main</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Title</h1>
+      <h3>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate,
+        deleniti.
+      </h3>
+    </>
+  );
 }
 
 export default App;
