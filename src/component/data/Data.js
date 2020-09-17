a011ecde9f12750e35d0160227b36737db306731
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-17 00:02:51 -0500

    section added usng map iterate

--- src/component/data/Data.js
+++ src/component/data/Data.js
@@ -3,45 +3,62 @@ export const MenuList = [
     id: 1,
     title: 'Food Title',
     img: 'img/menu/menu-1',
+    section: 'Category Name',
   },
   {
     id: 2,
     title: 'Food Title',
     img: 'img/menu/menu-2',
+    section: 'Category Name',
   },
   {
     id: 3,
     title: 'Food Title',
     img: 'img/menu/menu-3',
+    section: 'Category Name',
   },
   {
     id: 4,
     title: 'Food Title',
     img: 'img/menu/menu-4',
+    section: 'Category Name',
   },
   {
     id: 5,
     title: 'Food Title',
     img: 'img/menu/menu-5',
+    section: 'Category Name',
   },
   {
     id: 6,
     title: 'Food Title',
     img: 'img/menu/menu-6',
+    section: 'Category Name',
   },
   {
     id: 7,
     title: 'Food Title',
     img: 'img/menu/menu-7',
+    section: 'Category Name',
   },
   {
     id: 8,
     title: 'Food Title',
     img: 'img/menu/menu-8',
+    section: 'Category Name',
   },
   {
     id: 9,
     title: 'Food Title',
     img: 'img/menu/menu-9',
+    section: 'Category Name',
   },
 ];
+
+export const menus = MenuList.reduce((res, menu) => {
+  if (!res[menu.section]) {
+    res[menu.section] = [];
+  }
+  res[menu.section].push(menu);
+  return res;
+}, {});
--- src/component/section/menu/Menu.js
+++ src/component/section/menu/Menu.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import { MenuList } from '../../data/Data';
+import { menus } from '../../data/Data';
 import { MenuItem, MenuGrid, MenuLabel } from './MenuGrid';
 
 const MenuStyled = styled.div`
   height: 1000px;
-  margin: 5rem 5rem;
+  margin: 5rem 4rem;
 `;
 
 export function Menu() {
   return (
     <MenuStyled>
-      <MenuGrid>
-        {MenuList.map((menu) => (
-          <MenuItem img={menu.img}>
-            <MenuLabel>{menu.title}</MenuLabel>
-          </MenuItem>
-        ))}
-      </MenuGrid>
+      {Object.entries(menus).map(([sectionName, menus]) => (
+        <>
+          <h1>{sectionName}</h1>
+          <MenuGrid>
+            {menus.map((menu) => (
+              <MenuItem img={menu.img}>
+                <MenuLabel>{menu.title}</MenuLabel>
+              </MenuItem>
+            ))}
+          </MenuGrid>
+        </>
+      ))}
     </MenuStyled>
   );
 }
